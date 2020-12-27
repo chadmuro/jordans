84f47e9c2f8b5a37310e4af3e210161789c33d45
chadmuro <63533886+chadmuro@users.noreply.github.com>
2020-12-27 23:10:44 +0900

    link in header

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,13 @@
+import Link from 'next/link';
+
 const Header = () => {
-	return <header>History of Air Jordan</header>;
+	return (
+		<header>
+			<Link href="/">
+				<a>History of Air Jordan</a>
+			</Link>
+		</header>
+	);
 };
 
 export default Header;
